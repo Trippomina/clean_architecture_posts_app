94a1b1d61ac07ba5a50113eadbcaf6928be196f5
trippomina <trippomina@gmail.com>
2023-02-16 00:44:12 +0200

    project initialized

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_declarations, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+final primaryColor = Color(0xff082659);
+final secondaryColor = Color(0xff51eec2);
+
+final appTheme = ThemeData(
+    appBarTheme: AppBarTheme(backgroundColor: primaryColor, centerTitle: true),
+    brightness: Brightness.light,
+    primaryColor: primaryColor,
+    colorScheme: ColorScheme.light(primary: primaryColor),
+    progressIndicatorTheme: ProgressIndicatorThemeData(color: primaryColor),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: primaryColor, foregroundColor: secondaryColor),
+    inputDecorationTheme: InputDecorationTheme(
+        floatingLabelStyle: TextStyle(color: primaryColor),
+        iconColor: secondaryColor,
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: secondaryColor),
+            borderRadius: BorderRadius.circular(8))));
