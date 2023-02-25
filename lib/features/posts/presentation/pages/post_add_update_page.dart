e26feff84793300b85915c35e60de3f289e1b416
trippomina <trippomina@gmail.com>
2023-02-25 08:19:02 +0200

    Implemented the add & update post page (single shared page for both), form widget not ready yet

--- lib/features/posts/presentation/pages/post_add_update_page.dart
+++ lib/features/posts/presentation/pages/post_add_update_page.dart
@@ -3,7 +3,7 @@ import 'package:clean_architecture_posts_app/features/posts/domain/entities/post
 import 'package:clean_architecture_posts_app/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
 import 'package:clean_architecture_posts_app/features/posts/presentation/pages/posts_page.dart';
 import 'package:clean_architecture_posts_app/core/utils/snack_bar_message.dart';
-import 'package:clean_architecture_posts_app/features/posts/presentation/widgets/add_update_delete_post_page_widgets/form_widget.dart';
+import 'package:clean_architecture_posts_app/features/posts/presentation/widgets/add_update_post_page_widgets/form_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
